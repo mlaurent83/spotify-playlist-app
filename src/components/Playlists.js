6bee7beab76ce2b897dfcca4aebb4d50ac672463
Maeva Laurent-Estavel <maevaestavel@gmail.com>
2024-10-14 14:22:05 +0100

    added playlists page styling

--- src/components/Playlists.js
+++ src/components/Playlists.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Playlists.css';
 
 function Playlists() {
   return (
-    <div>
+    <div className='playlist-page'>
       <h1>Your Playlists</h1>
       <p>Here you'll see your playlists once we connect to Spotify API.</p>
       <Link to="/">Back to Home</Link>
