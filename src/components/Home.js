2eab238ebe5e69fea6cdd0e605a86263377e74a6
Maeva Laurent-Estavel <maevaestavel@gmail.com>
2024-10-14 14:16:39 +0100

    added stylesheet to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Home.css';
 
 function Home() {
   return (
-    <div>
+    <div className="home-container">
       <h1>Welcome to Spotify Playlist Creator</h1>
       <p>Create and share your favorite playlists!</p>
       <Link to="/playlists">View Playlists</Link>
