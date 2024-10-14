41c00addf82b185de30b52aff1ff4704de28ac76
Maeva Laurent-Estavel <maevaestavel@gmail.com>
2024-10-14 14:25:17 +0100

    added styling to users page

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Users.css'
 
 function Users() {
   return (
-    <div>
+    <div className="users-page">
       <h1>All Users</h1>
       <p>A page where you can see all users and their playlists.</p>
       <Link to="/">Back to Home</Link>
