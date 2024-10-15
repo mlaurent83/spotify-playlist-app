21ea140d2a4f6595c64c6b590ef49a5bdc4a0f1e
Maeva Laurent-Estavel <maevaestavel@gmail.com>
2024-10-15 13:40:54 +0100

    modified routes to render homepage when loading

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Playlists from './components/Playlists';
 import Songs from './components/Songs';
-import Users from './components/Users'
-import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
       <div className="App">
-      <Navbar />
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/playlists" element={<Playlists />} />
           <Route path="/songs" element={<Songs />} />
-          <Route path="/users" element={<Users />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </div>
     </Router>
