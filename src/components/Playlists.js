b57705fa54138376ddc798b0904d7b544261f081
Maeva Laurent-Estavel <maevaestavel@gmail.com>
2024-10-19 11:34:56 +0100

    added songs on playlist display

--- src/components/Playlists.js
+++ src/components/Playlists.js
@@ -14,7 +14,16 @@ function Playlists({ playlists }) {
           {playlists.map((playlist, index) => (
             <li key={index}>
               <h3>{playlist.name}</h3>
-              <p>{playlist.songs.length} songs</p>
+              <p>Your {playlist.name} playlist has {playlist.songs.length} songs!</p>
+              <div className="song-grid">
+            {playlist.songs.map((song) => (
+            <div key={song.url} className="song-card">
+              <img src={song.image[2]['#text'] || 'https://via.placeholder.com/150'} alt={song.name} />
+              <h3>{song.name}</h3>
+              <p>{song.artist.name}</p>
+            </div>
+          ))}
+        </div>
               {/* You can add more details or a link to view the full playlist */}
             </li>
           ))}
