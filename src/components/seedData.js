362f0a9db817473f9b320eab08ba11c95c89a9b2
Maeva Laurent-Estavel <maevaestavel@gmail.com>
2024-10-19 14:40:43 +0100

    added more users to seed file

--- src/components/seedData.js
+++ src/components/seedData.js
@@ -2,4 +2,10 @@ export const initialUsers = [
   { id: 1, first_name: 'Alice', last_name:"Smith", email: 'alice@example.com' },
   { id: 2, first_name:  'Bob',  last_name:"Williams", email: 'bob@example.com' },
   { id: 3, first_name: 'Charlie', last_name:"David", email: 'charlie@example.com' },
+  { id: 4, first_name: 'Audrey', last_name:"Ball", email: 'alice@example.com' },
+  { id: 5, first_name:  'Stewart',  last_name:"Hamilton", email: 'bob@example.com' },
+  { id: 6, first_name: 'Evan', last_name:"Glover", email: 'charlie@example.com' },
+  { id: 7, first_name: 'Diane', last_name:"Henderson", email: 'alice@example.com' },
+  { id: 8, first_name:  'Audrey',  last_name:"Mackay", email: 'bob@example.com' },
+  { id: 9, first_name: 'Ruth', last_name:"White", email: 'charlie@example.com' },
 ];
